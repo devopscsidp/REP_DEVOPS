ddc3c8a34ccaf478fdf23544f92f5fbba04813eb
Saurav Kundu <saurav.kundu@ericsson.com>
2018-02-14 15:46:03 +0530

    directory code

---
+++ src/main/java/Devops_task/devops/App.java
@@ -0,0 +1,27 @@
+package Devops_task.devops;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {	
+    	Scanner in = new Scanner(System.in);
+        String dirPath = in.nextLine();
+    	//String dirPath = "C:\\Metadata";
+    	File dir = new File(dirPath);
+    	String[] files = dir.list();
+    	if (files.length == 0) {
+    	    System.out.println("The directory is empty");
+    	} else {
+    	    for (String aFile : files) {
+    	        System.out.println(aFile);
+    	    }
+    	}
+    }
+}
